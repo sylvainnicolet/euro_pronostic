9e58adf42c1895dc2e327a46d5963b5b0523b962
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-06-02 17:53:57 +0200

    Calculate groups points

--- src/Controller/GroupeController.php
+++ src/Controller/GroupeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
 use App\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,20 +17,88 @@ class GroupeController extends AbstractController
   public function index(): Response
   {
     $teams = $this->getDoctrine()->getRepository(Team::class)->findAll();
+    $gamesPlayed = $this->getDoctrine()->getRepository(Game::class)->findBy([
+      'isFinished' => true,
+      'isGroupGame' => true
+    ]);
+
     $groupes = ['A', 'B', 'C', 'D', 'E', 'F'];
     $groupesOutput = [];
 
     for ($i = 0; $i < count($groupes); $i++) {
 
       foreach ($teams as $key => $team) {
+
+        // Game played
+        $calculatedPoints = $this->calculatePoints($team, $gamesPlayed);
+        $team->mj = $calculatedPoints['mj'];
+        $team->g = $calculatedPoints['g'];
+        $team->n = $calculatedPoints['n'];
+        $team->p = $calculatedPoints['p'];
+        $team->total = $calculatedPoints['total'];
+
         if ($team->getGroupe() == $i + 1) {
           $groupesOutput['Groupe ' . $groupes[$i]][$key] = $team;
         }
       }
     }
 
+//    dd($groupesOutput);
+
     return $this->render('user/groupes/index.html.twig', [
         'groupes' => $groupesOutput
     ]);
+  }
+
+  private function calculatePoints($team, $gamesPlayed) {
+    $mj = 0;
+    $g = 0;
+    $n = 0;
+    $p = 0;
+
+    foreach ($gamesPlayed as $game) {
+
+      // Si équipe 1
+      if ($game->getTeam1()->getName() == $team->getName()) {
+        $mj = $mj + 1;
+
+        // Null
+        if ($game->getScore1() > $game->getScore2()) {
+          $g = $g + 1;
+        }
+        elseif ($game->getScore1() == $game->getScore2()) {
+          $n = $n +1;
+        }
+        else {
+          $p = $p +1;
+        }
+      }
+
+      // Si équipe 2
+      if ($game->getTeam2()->getName() == $team->getName()) {
+        $mj = $mj + 1;
+
+        // Null
+        if ($game->getScore2() > $game->getScore1()) {
+          $g = $g + 1;
+        }
+        elseif ($game->getScore2() == $game->getScore1()) {
+          $n = $n +1;
+        }
+        else {
+          $p = $p +1;
+        }
+      }
+    }
+
+    $total = ($g * 3) + ($n * 1);
+
+    return [
+        'mj' => $mj,
+        'g' => $g,
+        'n' => $n,
+        'p' => $p,
+        'total' => $total
+    ];
   }
 }
