41990eba331605c313bdfdebbe0ffa5c1f79139e
Sylvain Nicolet <nicoletsylvain@gmail.com>
2021-06-04 15:42:09 +0200

    Sort ranking by total

--- src/Controller/RankingController.php
+++ src/Controller/RankingController.php
@@ -17,39 +17,13 @@ class RankingController extends AbstractController
    */
   public function index(): Response
   {
-    $allUsers = $this->getDoctrine()->getRepository(User::class)->findAll();
-    $users = [];
+    $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
-    // Get only users with role_user
-    foreach ($allUsers as $user) {
-      if (in_array("ROLE_USER", $user->getRoles())) {
-        array_push($users, $user);
-      }
-    }
-
-    foreach ($users as $user) {
-      $composition = $this->getDoctrine()->getRepository(Composition::class)->findOneBy([
-          'player' => $user->getId()
-      ]);
-      $gamesPlayed = $this->getDoctrine()->getRepository(Game::class)->findBy(['isFinished' => true,]);
-
-      if ($composition != null) {
-        $team1 = $this->calculatePoints($composition->getTeam1(), $gamesPlayed, 8);
-        $team2 = $this->calculatePoints($composition->getTeam2(), $gamesPlayed, 7);
-        $team3 = $this->calculatePoints($composition->getTeam3(), $gamesPlayed, 6);
-        $team4 = $this->calculatePoints($composition->getTeam4(), $gamesPlayed, 5);
-        $team5 = $this->calculatePoints($composition->getTeam5(), $gamesPlayed, 4);
-        $team6 = $this->calculatePoints($composition->getTeam6(), $gamesPlayed, 3);
-        $team7 = $this->calculatePoints($composition->getTeam7(), $gamesPlayed, 2);
-        $team8 = $this->calculatePoints($composition->getTeam8(), $gamesPlayed, 1);
-        $user->getComposition()->total = $team1 + $team2 + $team3 + $team4 + $team5 + $team6 + $team7 + $team8;
-      }
-
-    }
+    $data = $this->fillData($users);
 
     return $this->render('user/ranking/index.html.twig', [
         'title' => 'Classement général',
-        'users' => $users
+        'data' => $data
     ]);
   }
 
@@ -59,36 +33,56 @@ class RankingController extends AbstractController
    */
   public function indexLeague(): Response
   {
-    $user = $this->getUser();
-
     $users = $this->getDoctrine()->getRepository(User::class)->findBy([
-        'league' => $user->getLeague()
+        'league' => $this->getUser()->getLeague()
     ]);
 
-    foreach ($users as $user) {
-      $composition = $this->getDoctrine()->getRepository(Composition::class)->findOneBy([
-          'player' => $user->getId()
-      ]);
-      $gamesPlayed = $this->getDoctrine()->getRepository(Game::class)->findBy(['isFinished' => true,]);
-
-      if ($composition != null) {
-        $team1 = $this->calculatePoints($composition->getTeam1(), $gamesPlayed, 8);
-        $team2 = $this->calculatePoints($composition->getTeam2(), $gamesPlayed, 7);
-        $team3 = $this->calculatePoints($composition->getTeam3(), $gamesPlayed, 6);
-        $team4 = $this->calculatePoints($composition->getTeam4(), $gamesPlayed, 5);
-        $team5 = $this->calculatePoints($composition->getTeam5(), $gamesPlayed, 4);
-        $team6 = $this->calculatePoints($composition->getTeam6(), $gamesPlayed, 3);
-        $team7 = $this->calculatePoints($composition->getTeam7(), $gamesPlayed, 2);
-        $team8 = $this->calculatePoints($composition->getTeam8(), $gamesPlayed, 1);
-        $user->getComposition()->total = $team1 + $team2 + $team3 + $team4 + $team5 + $team6 + $team7 + $team8;
-      }
-
-    }
+    $data = $this->fillData($users);
 
     return $this->render('user/ranking/index.html.twig', [
         'title' => 'Classement de ma ligue',
-        'users' => $users
+        'data' => $data
     ]);
+  }
+
+  private function fillData($users) {
+    $data = [];
+    foreach ($users as $key => $user) {
+
+      // Get only users with role_user
+      if (in_array("ROLE_USER", $user->getRoles())) {
+
+        $data['users'][$key]['id'] = $user->getId();
+        $data['users'][$key]['slug'] = $user->getSlug();
+        $data['users'][$key]['username'] = $user->getUsername();
+        $data['users'][$key]['league'] = $user->getLeague()->getName();
+
+        if ($user->getComposition() == null) {
+          $data['users'][$key]['total'] = 0;
+          $data['users'][$key]['hasComposition'] = false;
+        } else {
+
+          $composition = $this->getDoctrine()->getRepository(Composition::class)->findOneBy([
+              'player' => $user->getId()
+          ]);
+          $gamesPlayed = $this->getDoctrine()->getRepository(Game::class)->findBy(['isFinished' => true,]);
+
+          if ($composition != null) {
+            $team1 = $this->calculatePoints($composition->getTeam1(), $gamesPlayed, 8);
+            $team2 = $this->calculatePoints($composition->getTeam2(), $gamesPlayed, 7);
+            $team3 = $this->calculatePoints($composition->getTeam3(), $gamesPlayed, 6);
+            $team4 = $this->calculatePoints($composition->getTeam4(), $gamesPlayed, 5);
+            $team5 = $this->calculatePoints($composition->getTeam5(), $gamesPlayed, 4);
+            $team6 = $this->calculatePoints($composition->getTeam6(), $gamesPlayed, 3);
+            $team7 = $this->calculatePoints($composition->getTeam7(), $gamesPlayed, 2);
+            $team8 = $this->calculatePoints($composition->getTeam8(), $gamesPlayed, 1);
+            $data['users'][$key]['total'] = $team1 + $team2 + $team3 + $team4 + $team5 + $team6 + $team7 + $team8;
+            $data['users'][$key]['hasComposition'] = true;
+          }
+        }
+      }
+    }
+    return $data;
   }
 
   private function calculatePoints($team, $gamesPlayed, $coeff) {
